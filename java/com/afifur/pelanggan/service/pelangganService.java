04285a41b928c250f4b031fe4c40d682f30df361
AfifurRaihan <113502491+AfifurRaihan@users.noreply.github.com>
2022-11-14 20:17:52 -0800

    Add files via upload

---
+++ java/com/afifur/pelanggan/service/pelangganService.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.afifur.pelanggan.service;
+
+import com.afifur.pelanggan.VO.pelanggan;
+
+/**
+ *
+ * @author ASUS
+ */
+public class pelangganService {
+
+    public pelanggan savePelanggan(pelanggan pelanggan) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public pelanggan findPelangganById(Long pelangganId) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
